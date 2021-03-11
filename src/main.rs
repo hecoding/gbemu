5488f674054f4b5911d23c9ea794d42a0bc0b577
hecoding <hector_laria@hotmail.com>
2021-03-11 02:42:37 +0100

    Add memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::prelude::*;
+use std::fs::File;
+
+struct Memory {
+    cart: Vec<u8>
+}
+
+impl Memory {
+    fn new(filepath: &str) -> Memory {
+        let mut f = File::open(filepath)
+            .expect("Failed to read file");
+        let mut cart = Vec::new();
+        f.read_to_end(&mut cart).expect("Error reading data");
+        cart.shrink_to_fit();
+
+        Memory {
+            cart
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mem = Memory::new("roms/Tetris (World) (Rev A).gb");
+    println!("{:?}", &mem.cart[0..10]);
 }
